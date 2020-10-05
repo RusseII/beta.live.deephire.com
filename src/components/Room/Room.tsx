3cec7133d0b14f342a4ca8b0bd04f54c13fa7dc3
russell <russell@deephire.com>
2020-10-05 12:51:46 -0400

    use makeStyles to dynamically change grid

--- src/components/Room/Room.tsx
+++ src/components/Room/Room.tsx
@@ -1,27 +1,50 @@
 import React from 'react';
 import ParticipantList from '../ParticipantList/ParticipantList';
-import { styled } from '@material-ui/core/styles';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import clsx from 'clsx';
 import MainParticipant from '../MainParticipant/MainParticipant';
 import SideBar from '../SideBar';
+import { useCandidate } from '../../hooks/useLive';
 
-const Container = styled('div')(({ theme }) => ({
-  position: 'relative',
-  height: '100%',
-  display: 'grid',
-  gridTemplateColumns: `1fr ${theme.sidebarWidth}px`,
-  gridTemplateRows: '100%',
-  [theme.breakpoints.down('md')]: {
-    gridTemplateColumns: `100%`,
-    gridTemplateRows: `1fr ${theme.sidebarMobileHeight + 16}px`,
+const useStyles = makeStyles((theme: Theme) => ({
+  room: {
+    position: 'relative',
+    height: '100%',
+    display: 'grid',
+  },
+  noFiles: {
+    gridTemplateColumns: `1fr ${theme.sidebarWidth}px`,
+    gridTemplateRows: '100%',
+    [theme.breakpoints.down('md')]: {
+      gridTemplateColumns: `100%`,
+      gridTemplateRows: `1fr ${theme.sidebarMobileHeight + 16}px`,
+    },
+  },
+  files: {
+    gridTemplateColumns: `1fr ${theme.sidebarWidth}px`,
+    gridTemplateRows: '100%',
+    [theme.breakpoints.down('md')]: {
+      gridTemplateColumns: `100%`,
+      gridTemplateRows: `1fr ${theme.sidebarMobileHeight + 16}px`,
+    },
   },
 }));
 
 export default function Room() {
+  const { data } = useCandidate();
+  // const files = data?.files;
+  const classes = useStyles();
+
   return (
-    <Container>
+    <div
+      className={clsx(classes.room, {
+        [classes.noFiles]: true,
+        [classes.files]: false,
+      })}
+    >
       <MainParticipant />
       <ParticipantList />
-      {/* <SideBar /> */}
-    </Container>
+      {/* {files && <SideBar />} */}
+    </div>
   );
 }
